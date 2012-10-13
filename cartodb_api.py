0f6e5c5c80cc4f24f2cd72ece952d2039b01ce5b
mik3cap <info@brainewave.com>
2012-10-13 18:03:02 -0400

    Edit cartodb api version to include api key

--- cartodb_api.py
+++ cartodb_api.py
@@ -1,10 +1,12 @@
 from cartodb import CartoDBAPIKey, CartoDBException
 
-user =  'me@mail.com'
-API_KEY ='YOUR_CARTODB_API_KEY'
-cartodb_domain = 'YOUR_CARTODB_DOMAIN'
+user =  'wmaio@gmail.com'
+API_KEY ='ef51a53f44eff0538e1372e9cd955727c5afda38'
+cartodb_domain = 'wmaiouiru'
+
 cl = CartoDBAPIKey(API_KEY, cartodb_domain)
+
 try:
-    print cl.sql('select * from mytable')
+    print cl.sql('SELECT * FROM uiru_test1')
 except CartoDBException as e:
     print ("some error ocurred", e)
